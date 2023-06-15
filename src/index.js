aa4b0b5280c872b77c992835a4fab64706df3030
mateonlienard <mateonlienard@gmail.com>
2023-06-15 19:58:21 -0300

    agregue handlebars y socket a mi proyecto

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 const express = require("express");
 const PORT = 8080;
 const routes = require("./routes");
+const http = require('http').Server(app);
+const Io = require('socket.io')(http);
+const exphbs = require('express-handlebars');
+
 
 class Server {
     constructor(){
       this.app = express();
       this.settings();
       this.routes();
+      this.io = Io()
     }
   
     settings(){
       this.app.use(express.json());
       this.app.use(express.urlencoded({extended:true}));
     }
-  
+
+    socketIo(){
+      this.io.on('connection', (socket)=>{
+
+      });
+    }
+    
+    views(){
+      this.app.set('views', './views');
+      this.app.engine('handlebars', exphbs.engine());
+      this.app.set('view engine', 'handlebars');
+    }
+
+    public(){
+      this.app.use(express.static('./public'))
+    }
+
     routes(){
       routes(this.app);
     }
